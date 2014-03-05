8338d6dd0629e74734f10dddd27303674b56d40d
edwardwu7 <wuzhiyuan7@gmail.com>
2014-03-05 23:55:49 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Edward::Application.config.secret_key_base = '9057ab3b4b25616b2fd71357cf91075dd4e70f69a9fb15fd9cee5708cce5ddd1c666f1dd1241ef4275e382c09acf229801ec22d25b9defff30e990938e43c87e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Edward::Application.config.session_store :cookie_store, key: '_edward_session'
